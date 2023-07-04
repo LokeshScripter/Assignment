7b829e12dea02f8c6436023a3648c16478df9e38
LokeshScripter <138587629+LokeshScripter@users.noreply.github.com>
2023-07-04 18:20:32 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,26 @@
+
+
+
+def adder(a,b):
+    old_number = 0
+    for number in range(a,b):
+        sum = old_number + number
+        print(sum)
+        old_number = number
+
+
+adder(3,7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
